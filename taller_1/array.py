8aeb9b9f355a0749282fadf2f36fb7e2789f00cc
Josuram-xd <joseph.herrera@campusucc.edu.co>
2026-08-13 23:10:12 +0000

    1

---
+++ taller_1/array.py
@@ -0,0 +1,7 @@
+days = ["lunes", "martes", "miercoles", "jueves", "viernes"]
+
+paswords = [
+    ["123", "holi12", "zorras"],
+    ["456", "kundial", "contraseña"],
+    ["perro128", "matacocis", "codigouwu"]
+]
